84168a6eb248fc079029c46f88f821cbe9442995
Bretton Vine <bretton@hivemind.net>
2019-01-05 23:16:54 +0200

    fixing bad merge

--- official-http/python-swaggerpy/setup.py
+++ official-http/python-swaggerpy/setup.py
@@ -4,19 +4,11 @@ from setuptools import setup
 
 
 setup(name='bitmex',
-<<<<<<< HEAD
       version='0.2.2',
       description='BitMEX API client.',
       author='Samuel Reed',
       author_email='sam@bitmex.com',
       url='https://github.com/bitmex/api-Connectors',
-=======
-      version='0.2.1',
-      description='BitMEX API client.',
-      author='Samuel Reed',
-      author_email='sam@bitmex.com',
-      url='https://github.com/bitmex/apiConnectors',
->>>>>>> 862467f... updated for python3, greatly expanded README
       install_requires=['bravado'],
       packages=[''],
       )
