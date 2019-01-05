bd67b8d75aa93eadb3aeab5114ad5be4ba63136c
Bretton Vine <bretton@hivemind.net>
2019-01-05 23:35:07 +0200

    adding personal touches

--- official-ws/delta-server/run.sh
+++ official-ws/delta-server/run.sh
@@ -1,5 +1,8 @@
 #!/bin/bash -e
 
+# optional custom config I need in source, enable this if using supervisord to start
+# cd ~/api-connectors/official-ws/delta-server
+
 [ ! -f config.js ] && echo 'Place your config in config.js!' && exit 1;
 
 trap finish EXIT
